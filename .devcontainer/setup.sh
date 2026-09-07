2cb9cde1b1ca92d8b5ad092a9ee5b8c39d929f9b
Baraka Maderisha Winny <72584032+benhimarthi@users.noreply.github.com>
2026-09-07 21:41:32 +0000

    Add devcontainer setup for Flutter + Android SDK

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+cd ~
+
+# Install Flutter SDK
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Android command-line SDK tools
+if [ ! -d "android-sdk" ]; then
+  mkdir -p android-sdk/cmdline-tools
+  cd android-sdk/cmdline-tools
+  curl -o cmdline-tools.zip https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+  unzip -q cmdline-tools.zip
+  mv cmdline-tools latest
+  rm cmdline-tools.zip
+  cd ~
+fi
+
+export ANDROID_HOME="$HOME/android-sdk"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools"
+
+yes | sdkmanager --licenses
+sdkmanager "platform-tools" "platforms;android-36" "build-tools;28.0.3"
+
+flutter config --android-sdk "$ANDROID_HOME"
+
+# Make PATH persistent in interactive shells too
+echo 'export PATH="$PATH:$HOME/flutter/bin:$HOME/android-sdk/cmdline-tools/latest/bin:$HOME/android-sdk/platform-tools"' >> ~/.bashrc
+echo 'export ANDROID_HOME="$HOME/android-sdk"' >> ~/.bashrc
+
+flutter doctor
